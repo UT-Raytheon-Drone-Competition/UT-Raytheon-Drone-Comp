209cbc3474be60232df307b06647ad74064b19f4
Jetson Nano <jetson-nano-ut@gmail.com>
2022-03-08 17:44:40 -0600

    Landing successful

---
+++ challenges/challenge-1/include/lib.hpp
@@ -0,0 +1,46 @@
+#ifndef GNC_FUNCTIONS
+#define GNC_FUNCTIONS
+
+#include <ros/ros.h> 
+#include <geometry_msgs/PoseStamped.h> 
+#include <mavros_msgs/CommandTOL.h>
+#include <mavros_msgs/CommandBool.h> 
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
+#include <nav_msgs/Odometry.h>
+#include <vector>
+
+nav_msgs::Odometry current_pose_g;
+
+bool check_waypoint_reached(geometry_msgs::PoseStamped goal, float pos_tolerance=1, float heading_tolerance=0.01) {
+
+    // Check for correct position 
+    float deltaX = abs(goal.pose.position.x - current_pose_g.pose.pose.position.x);
+    float deltaY = abs(goal.pose.position.y - current_pose_g.pose.pose.position.y);
+    float deltaZ = abs(goal.pose.position.z - current_pose_g.pose.pose.position.z);
+    float dMag = sqrt( pow(deltaX, 2) + pow(deltaY, 2) + pow(deltaZ, 2) );
+
+    // ROS_INFO("dMag %f", dMag);
+    // ROS_INFO("current pose x %F y %f z %f", (current_pose_g.pose.pose.position.x), (current_pose_g.pose.pose.position.y), (current_pose_g.pose.pose.position.z));
+    // ROS_INFO("waypoint pose x %F y %f z %f", waypoint_g.pose.position.x, waypoint_g.pose.position.y,waypoint_g.pose.position.z);
+    //check orientation
+    // float cosErr = cos(current_heading_g*(M_PI/180)) - cos(local_desired_heading_g*(M_PI/180));
+    // float sinErr = sin(current_heading_g*(M_PI/180)) - sin(local_desired_heading_g*(M_PI/180));
+    
+    // float headingErr = sqrt( pow(cosErr, 2) + pow(sinErr, 2) );
+
+    // ROS_INFO("current heading %f", current_heading_g);
+    // ROS_INFO("local_desired_heading_g %f", local_desired_heading_g);
+    // ROS_INFO("current heading error %f", headingErr);
+
+    if( dMag < pos_tolerance /*&& headingErr < heading_tolerance*/)
+	return 1;
+    else
+	return 0;
+}
+
+void pos_callback(const nav_msgs::Odometry::ConstPtr& msg) {
+    current_pose_g = *msg;
+}
+
+#endif //GNC_FUNCTIONS
--- challenges/challenge-1/src/fly.cpp
+++ challenges/challenge-1/src/fly.cpp
@@ -1,81 +1,118 @@
-#include <gnc_functions.hpp>
-//include API 
+#include "lib.hpp"
 
-int main(int argc, char** argv)
-{
-	//initialize ros 
-	ros::init(argc, argv, "gnc_node");
-	ros::NodeHandle gnc_node("~");
-	
-	//initialize control publisher/subscribers
-	init_publisher_subscriber(gnc_node);
+mavros_msgs::State current_state;
 
-  	// wait for FCU connection
-	wait4connect();
+void state_cb(const mavros_msgs::State::ConstPtr& msg){
+    current_state = *msg;
+}
 
-	//wait for used to switch to mode GUIDED
-	wait4start();
+int main(int argc, char **argv) {
 
-	//create local reference frame 
-	initialize_local_frame();
+    ros::init(argc, argv, "offb_node");
+    ros::NodeHandle nh;
 
-	//request takeoff
-	takeoff(3);
+    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
+            ("mavros/state", 10, state_cb);
+    ros::Subscriber pose_sub = nh.subscribe<nav_msgs::Odometry>
+            ("mavros/global_position/local", 10, pos_callback);
+    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
+            ("mavros/setpoint_position/local", 10);
+    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
+            ("mavros/cmd/arming");
+    ros::ServiceClient land_client = nh.serviceClient<mavros_msgs::CommandTOL>
+	    ("mavros/cmd/land");
+    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
+            ("mavros/set_mode");
 
-	//specify some waypoints 
-	std::vector<gnc_api_waypoint> waypointList;
-	gnc_api_waypoint nextWayPoint;
-	nextWayPoint.x = 0;
-	nextWayPoint.y = 0;
-	nextWayPoint.z = 3;
-	nextWayPoint.psi = 0;
-	waypointList.push_back(nextWayPoint);
-	nextWayPoint.x = 5;
-	nextWayPoint.y = 0;
-	nextWayPoint.z = 3;
-	nextWayPoint.psi = -90;
-	waypointList.push_back(nextWayPoint);
-	nextWayPoint.x = 5;
-	nextWayPoint.y = 5;
-	nextWayPoint.z = 3;
-	nextWayPoint.psi = 0;
-	waypointList.push_back(nextWayPoint);
-	nextWayPoint.x = 0;
-	nextWayPoint.y = 5;
-	nextWayPoint.z = 3;
-	nextWayPoint.psi = 90;
-	waypointList.push_back(nextWayPoint);
-	nextWayPoint.x = 0;
-	nextWayPoint.y = 0;
-	nextWayPoint.z = 3;
-	nextWayPoint.psi = 180;
-	waypointList.push_back(nextWayPoint);
-	nextWayPoint.x = 0;
-	nextWayPoint.y = 0;
-	nextWayPoint.z = 3;
-	nextWayPoint.psi = 0;
-	waypointList.push_back(nextWayPoint);
+    // The setpoint publishing rate MUST be faster than 2Hz
+    ros::Rate rate(20.0);
 
+    // Wait for FCU connection
+    while(ros::ok() && !current_state.connected){
+        ros::spinOnce();
+        rate.sleep();
+    }
 
-	//specify control loop rate. We recommend a low frequency to not over load the FCU with messages. Too many messages will cause the drone to be sluggish
-	ros::Rate rate(2.0);
-	int counter = 0;
-	while(ros::ok())
-	{
-		ros::spinOnce();
-		rate.sleep();
-		if(check_waypoint_reached(.3) == 1)
-		{
-			if (counter < waypointList.size())
-			{
-				set_destination(waypointList[counter].x,waypointList[counter].y,waypointList[counter].z, waypointList[counter].psi);
-				counter++;	
-			}else{
-				//land after all waypoints are reached
-				land();
-			}	
-		}	
-		
+    // Land command message
+    mavros_msgs::CommandTOL land_cmd;
+    land_cmd.request.yaw = 0;
+    land_cmd.request.latitude = 0;
+    land_cmd.request.longitude = 0;
+    land_cmd.request.altitude = 0;
+
+    // Waypoints
+    geometry_msgs::PoseStamped pose1;
+    pose1.pose.position.x = 0;
+    pose1.pose.position.y = 0;
+    pose1.pose.position.z = 6;
+    geometry_msgs::PoseStamped pose2;
+    pose2.pose.position.x = 0;
+    pose2.pose.position.y = 30;
+    pose2.pose.position.z = 6;
+
+    std::vector<geometry_msgs::PoseStamped*> goals;
+    goals.push_back(&pose1);
+    goals.push_back(&pose2);
+
+    // Send a few setpoints before starting
+    for(int i = 100; ros::ok() && i > 0; --i){
+        local_pos_pub.publish(pose1);
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    // Set drone to Offboard mode
+    mavros_msgs::SetMode offb_set_mode;
+    offb_set_mode.request.custom_mode = "OFFBOARD";
+
+    // Arm drone
+    mavros_msgs::CommandBool arm_cmd;
+    arm_cmd.request.value = true;
+
+    ros::Time last_request = ros::Time::now();
+    int cur_goal_idx = 0;
+
+    //bool missionDone = false;
+
+    while(ros::ok()){
+        if( current_state.mode != "OFFBOARD" &&
+            (ros::Time::now() - last_request > ros::Duration(5.0))){
+            if( set_mode_client.call(offb_set_mode) &&
+                offb_set_mode.response.mode_sent){
+                ROS_INFO("Offboard enabled");
+            }
+            last_request = ros::Time::now();
+        } else {
+            if( !current_state.armed &&
+                (ros::Time::now() - last_request > ros::Duration(5.0))){
+                if( arming_client.call(arm_cmd) &&
+                    arm_cmd.response.success){
+                    ROS_INFO("Vehicle armed");
+                }
+                last_request = ros::Time::now();
+            }
+        }
+
+        local_pos_pub.publish(*(goals[cur_goal_idx]));
+	if(check_waypoint_reached(*(goals[cur_goal_idx]))){
+	    ROS_INFO_STREAM("Waypoint" << cur_goal_idx << " reached");
+	    cur_goal_idx++;	
+	    if(cur_goal_idx >= goals.size()) {
+		    ROS_INFO("Landing");
+		    //land();
+		    while (!(land_client.call(land_cmd) && land_cmd.response.success)) {
+		      //local_pos_pub.publish(pose);
+		      ROS_INFO("tring to land");
+		      ros::spinOnce();
+		      rate.sleep();
+    		    }
+		    //missionDone = true;
+	    }
 	}
-	return 0;
+
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    return 0;
 }
