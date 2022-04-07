b830f1f5c7c54dcade4c7dd2628f8b3ff5aee0b3
Emily Liu <emily2000liu@utexas.edu>
2022-04-07 18:47:35 -0500

    Updated landing controller

--- challenges/landing-controller/src/LandingController.cpp
+++ challenges/landing-controller/src/LandingController.cpp
@@ -6,7 +6,7 @@ LandingController::LandingController(ros::NodeHandle& nh, double xy_gain){
     done = false;
     altitude_sub = nh.subscribe<sensor_msgs::Range>("/sonar", 1,
                     &LandingController::altitude_cb, this);
-    velocity_pub = nh.advertise<geometry_msgs::Twist>("/mavros/setpoint_velocity/cmd_vel_unstamped",1);
+    velocity_pub = nh.advertise<geometry_msgs::Twist>("/mavros/setpoint_velocity/cmd_vel",1);
     land_client = nh.serviceClient<mavros_msgs::CommandTOL>("mavros/cmd/land");
     land_client.waitForExistence(ros::Duration(0.5));
     this->xy_gain = xy_gain;
@@ -24,7 +24,7 @@ void LandingController::update(double x_error, double y_error) {
     // Find location of line/marker
     double error_norm = sqrt(pow(x_error,2)+pow(y_error,2));
     if(altitude < LAND_HEIGHT && error_norm < ERROR_THRESHOLD){
-
+        ROS_INFO("Calling land client");
         land_client.call(landCmd);
         done = true;
         return;
