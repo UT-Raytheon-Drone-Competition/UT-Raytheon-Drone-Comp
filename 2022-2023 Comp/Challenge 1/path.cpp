1f18f97176ace719f2c875fb4147ba12a26a5bb2
aoj19037 <aoj19037@isp02.tacc.utexas.edu>
2023-01-24 10:47:44 -0600

    GNC functions and challenge one path

--- 2022-2023 Comp/Challenge 1/path.cpp
+++ 2022-2023 Comp/Challenge 1/path.cpp
@@ -1,1 +1,20 @@
-//test
+#inlcude "gns_functions.hpp"
+
+int main (int argc, char** argv)
+{
+  ros::init(argc, argv, "gnc_node");
+  ros::NodeHandle gnc_node;
+
+  init_publisher_subscriber(gnc_node);
+
+  //wait to connect with FCU
+  wait4connect();
+  //hold program until unyil piloy executes the program by switching the fcu flight mode to guided. Can be done in QGC
+  wait4start();
+  
+  initialize_local_frame();
+  takeoff(3);
+
+  //create waypoints
+
+}
