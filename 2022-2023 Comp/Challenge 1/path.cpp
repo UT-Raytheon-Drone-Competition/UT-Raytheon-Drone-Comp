284f5069e8b8bc6bf5ad0d4f28ccf57db18165f7
aoj19037 <aoj19037@isp02.tacc.utexas.edu>
2023-01-26 00:37:14 -0600

    Completed path for first challenge

--- 2022-2023 Comp/Challenge 1/path.cpp
+++ 2022-2023 Comp/Challenge 1/path.cpp
@@ -1,20 +1,100 @@
-#inlcude "gns_functions.hpp"
+#include <gnc_functions.hpp>
+//include API 
 
-int main (int argc, char** argv)
+int main(int argc, char** argv)
 {
+  //initialize ros 
   ros::init(argc, argv, "gnc_node");
   ros::NodeHandle gnc_node;
-
+  
+  //initialize control publisher/subscribers
   init_publisher_subscriber(gnc_node);
 
-  //wait to connect with FCU
+  // wait for FCU connection
   wait4connect();
-  //hold program until unyil piloy executes the program by switching the fcu flight mode to guided. Can be done in QGC
+
+  //wait for used to switch to mode GUIDED
   wait4start();
-  
+
+  //create local reference frame 
   initialize_local_frame();
-  takeoff(3);
 
-  //create waypoints
+  //request takeoff
+  takeoff(10);
 
+  //specify some waypoints
+  //drone looking south, starts at 0,0,0,0
+  double xPos = 0;
+  double yPos = 0;
+  dobule zPos = 7.32;//8 yards -> meters Search 8*8 area
+  std::vector<gnc_api_waypoint> waypointList;
+  gnc_api_waypoint nextWayPoint;
+  nextWayPoint.x = xPos;
+  nextWayPoint.y = yPos;
+  nextWayPoint.z = zPos;
+  nextWayPoint.psi = 0;
+  waypointLiar.push_back(nextWayPoint);
+  nextWayPoint.x = -4;//align with first ugv row 
+  nextWayPoint.y = 4;
+  nextWayPoint.z = zPos;
+  nextWayPoint.psi = 0;
+  waypointLiar.push_back(nextWayPoint);
+  if (int i = 1; (xPos-4) < 50; i++)
+    {
+      if (i%2 == 0)
+	{
+	  waypointLiar.push_back(nextWayPoint);
+	  nextWayPoint.x = xPos;
+	  nextWayPoint.y = yPos-42;
+	  nextWayPoint.z = zPos;
+	  nextWayPoint.psi = 0;
+	  waypointLiar.push_back(nextWayPoint);
+	  nextWayPoint.x = xPos-4;
+	  nextWayPoint.y = yPos;
+	  nextWayPoint.z = zPos;
+	  nextWayPoint.psi = 0;
+	  waypointLiar.push_back(nextWayPoint);
+	}
+      else 
+	{
+	  nextWayPoint.x = xPos;
+	  nextWayPoint.y = yPos+42;
+	  nextWayPoint.z = zPos;
+	  nextWayPoint.psi = 0;
+	  waypointLiar.push_back(nextWayPoint);
+	  nextWayPoint.x = xPos-4;
+	  nextWayPoint.y = yPos;
+	  nextWayPoint.z = zPos;
+	  nextWayPoint.psi = 0;
+	  waypointLiar.push_back(nextWayPoint);
+	}
+    }
+  
+  nextWayPoint.x = -51;//cross finish line
+  nextWayPoint.y = yPos;
+  nextWayPoint.z = 10;
+  nextWayPoint.psi = 0;
+  waypointList.push_back(nextWayPoint);//since drone at 9 feet sees about 9*9 it is better if we move to x= -4.5. Moving drone to 4.5 puts it in the middle of first ugv row 
+  
+  //specify control loop rate. We recommend a low frequency to not over load the FCU with messages. Too many messages will cause the drone to be sluggish
+  ros::Rate rate(2.0);
+  int counter = 0;
+  while(ros::ok())
+    {
+      ros::spinOnce();
+      rate.sleep();
+      if(check_waypoint_reached(.3) == 1)
+	{
+	  if (counter < waypointList.size())
+	    {
+	      set_destination(waypointList[counter].x,waypointList[counter].y,waypointList[counter].z, waypointList[counter].psi);
+	      counter++;
+	    }else{
+	    //land after all waypoints are reached
+	    land();
+	  }
+	}
+      
+    }
+  return 0;
 }
