623475536ca737a359c7be378514d85dc95d0c5f
Harrison Jin <harrison.jin@utexas.edu>
2023-02-23 21:08:37 -0600

    add world files, model files, and fix challenge1

--- 2023/challenge1/src/path.cpp
+++ 2023/challenge1/src/path.cpp
@@ -1,6 +1,12 @@
 #include <gnc_functions.hpp>
 //include API 
 
+const double ALTITUDE = 7.32;
+const double ROW_DIST = 3.66;
+const double COL_DIST = 38.4;
+const double MIN_X = -45.72;
+const double FINISH_LINE_X = -46.6;
+
 int main(int argc, char** argv)
 {
   //initialize ros 
@@ -17,62 +23,79 @@ int main(int argc, char** argv)
   wait4start();
 
   //create local reference frame 
-  // initialize_local_frame();
+  initialize_local_frame();
 
   //request takeoff
-  takeoff(2);
+  takeoff(ALTITUDE);
+
+/*
+
+<--- +x
+
+    |
+    |
+    v +y
+
+    +z is out of the page
+
+    --------------------
+    |     |     |     |
+    |     |    1 0    |
+    |     |     |     |
+    |     |     |     |
+    |     |     |     |
+    |     |     |     |
+    |     |     |     |
+    |     |     |     |
+    |     |     |     |
+    |     |     |     |
+    |     |    1 0    |
+    |     |     |     |
+    --------------------
+
+*/
+
 
   //specify some waypoints
   //drone looking south, starts at 0,0,0,0
-  // double xPos = 0;
-  // double yPos = 0;
-  // double zPos = 7.32;//8 yards -> meters Search 8*8 area
-  // std::vector<gnc_api_waypoint> waypointList;
-  // gnc_api_waypoint nextWayPoint;
-  // nextWayPoint.x = xPos;
-  // nextWayPoint.y = yPos;
-  // nextWayPoint.z = zPos;
-  // nextWayPoint.psi = 0;
-  // waypointList.push_back(nextWayPoint);
-  // nextWayPoint.x = -3.66;//align with first ugv row 
-  // nextWayPoint.y = 3.66;
-  // nextWayPoint.z = zPos;
-  // nextWayPoint.psi = 0;
-  // waypointList.push_back(nextWayPoint);
-  // for(int i = 1; (xPos-3.66) < 45.72; i++){
-  //   if (i%2 == 0){
-  //     waypointList.push_back(nextWayPoint);
-  //     nextWayPoint.x = xPos;
-  //     nextWayPoint.y = yPos-38.4;
-  //     nextWayPoint.z = zPos;
-  //     nextWayPoint.psi = 0;
-  //     waypointList.push_back(nextWayPoint);
-  //     nextWayPoint.x = xPos-3.66;
-  //     nextWayPoint.y = yPos;
-  //     nextWayPoint.z = zPos;
-  //     nextWayPoint.psi = 0;
-  //     waypointList.push_back(nextWayPoint);
-  //   }
-  //       else 
-  //   {
-  //     nextWayPoint.x = xPos;
-  //     nextWayPoint.y = yPos+38.4;
-  //     nextWayPoint.z = zPos;
-  //     nextWayPoint.psi = 0;
-  //     waypointList.push_back(nextWayPoint);
-  //     nextWayPoint.x = xPos-3.66;
-  //     nextWayPoint.y = yPos;
-  //     nextWayPoint.z = zPos;
-  //     nextWayPoint.psi = 0;
-  //     waypointList.push_back(nextWayPoint);
-  //   }
-  // }
+  double xPos = 0;
+  double yPos = 0;
+  double zPos = ALTITUDE;//8 yards -> meters Search 8*8 area
+  std::vector<gnc_api_waypoint> waypointList;
+  gnc_api_waypoint nextWayPoint;
+  nextWayPoint.x = xPos;
+  nextWayPoint.y = yPos;
+  nextWayPoint.z = zPos;
+  nextWayPoint.psi = 0;
+  waypointList.push_back(nextWayPoint);
+  xPos = -ROW_DIST;//align with first ugv row 
+  yPos = ROW_DIST;
+  nextWayPoint.x = xPos;
+  nextWayPoint.y = yPos;
+  waypointList.push_back(nextWayPoint);
+  for(int i = 1; (xPos-2*ROW_DIST) > MIN_X; i++){
+    if (i%2 == 0){
+      waypointList.push_back(nextWayPoint);
+      nextWayPoint.x = xPos;
+      nextWayPoint.y = (yPos-=COL_DIST);
+      waypointList.push_back(nextWayPoint);
+      nextWayPoint.x = (xPos-=ROW_DIST);
+      nextWayPoint.y = yPos;
+      waypointList.push_back(nextWayPoint);
+    }
+    else {
+      nextWayPoint.x = xPos;
+      nextWayPoint.y = (yPos+=COL_DIST);
+      waypointList.push_back(nextWayPoint);
+      nextWayPoint.x = (xPos-=ROW_DIST);
+      nextWayPoint.y = yPos;
+      waypointList.push_back(nextWayPoint);
+    }
+  }
   
-  // nextWayPoint.x = -46.6;//cross finish line
-  // nextWayPoint.y = yPos;
-  // nextWayPoint.z = 7.32;
-  // nextWayPoint.psi = 0;
-  // waypointList.push_back(nextWayPoint);//since drone at 9 feet sees about 9*9 it is better if we move to x= -4.5. Moving drone to 4.5 puts it in the middle of first ugv row 
+  nextWayPoint.x = FINISH_LINE_X;//cross finish line
+  nextWayPoint.y = yPos;
+  waypointList.push_back(nextWayPoint);//since drone at 9 feet sees about 9*9 it is better if we move to x= -4.5. Moving drone to 4.5 puts it in the middle of first ugv row 
   
   //specify control loop rate. We recommend a low frequency to not over load the FCU with messages. Too many messages will cause the drone to be sluggish
   ros::Rate rate(20);
@@ -81,17 +104,16 @@ int main(int argc, char** argv)
     {
       ros::spinOnce();
       rate.sleep();
-      // if(check_waypoint_reached(.3) == 1) {
-      //   if (counter < waypointList.size())
-      //     {
-      //       set_destination(waypointList[counter].x,waypointList[counter].y,waypointList[counter].z, waypointList[counter].psi);
-      //       counter++;
-      //     }else{
-      //     //land after all waypoints are reached
-      //     land();
-      //   }
-      // }
-      set_destination(0,0,2,0);
+      if(check_waypoint_reached(.3) == 1) {
+        if (counter < waypointList.size())
+          {
+            set_destination(waypointList[counter].x,waypointList[counter].y,waypointList[counter].z, waypointList[counter].psi);
+            counter++;
+          }else{
+          //land after all waypoints are reached
+          land();
+        }
+      }
       
     }
   return 0;
